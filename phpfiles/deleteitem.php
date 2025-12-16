ef2ffd5b2b3a9aaf04120ff8c58c278d1487e333
Shatha-gh <shathasaeed2025@gmail.com>
2025-12-16 15:00:22 +0200

    Update deleteitem.php

--- phpfiles/deleteitem.php
+++ phpfiles/deleteitem.php
@@ -1,7 +1,16 @@
 <?php
-session_start();
+
 $conn=pg_connect("host=localhost dbname=lost_found_db user=postgres password=14112006");
 $id_item=$_REQUEST['item_id'];
+
+$img_query="SELECT image FROM items WHERE id='".pg_escape_string($id_item)."' ";
+$result=pg_query($conn,$img_query);
+if($row=pg_fetch_assoc($result)){
+$img_name=$row['image'];
+$img_path="../images/". $img_name;
+}
+if(file_exists($img_path)){unlink($img_path); }
+
 pg_query($conn, "DELETE FROM items WHERE id='$id_item' ");
 header("location: ../discover-items.php");
 ?>
