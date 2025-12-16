8fa5c71176c19ef3302f70adcf9e2f23fc8cc6b8
Shatha-gh <shathasaeed2025@gmail.com>
2025-12-16 14:32:43 +0200

    Refactor additem.php for security and variable corrections
    Updated SQL query to use pg_escape_string for input sanitization and fixed variable usage.

--- phpfiles/additem.php
+++ phpfiles/additem.php
@@ -1,8 +1,8 @@
 <?php
 $conn=pg_connect("host=localhost dbname=lost_found_db user=postgres password=14112006");
+session_start();
+$email = $_SESSION['email'];
 
-$email=$_SESSION['email']
-$name=pg_query($conn,"SELECT name FROM items where name='$email'");
 $item_name=$_REQUEST['item_name'];
 $item_type=$_REQUEST['item_type'];
 $item_description=$_REQUEST['item_description'];
@@ -10,13 +10,22 @@ $item_location=$_REQUEST['item_location'];
 $item_date=$_REQUEST['item_date'];
 $user_number=$_REQUEST['user_number'];
 
-$target_dir="images/";
-$extension=pathinfo($_FILES['item_image']['name'], PATHINFO_EXTENSION);
-$img_name=$name."_".time()."_".$extension;
+$target_dir="../images/";
+
+$extension=strtolower(pathinfo($_FILES['item_image']['name'], PATHINFO_EXTENSION));
+$img_name=$email."_".time().".".$extension;
 $target_file=$target_dir.$img_name;
 
 move_uploaded_file($_FILES['item_image']['tmp_name'],$target_file);
-$sql="INSERT INTO items (user, email, name,type, description, location, date, number, image ) VALUES('$name', '$email', '$item_name','item_type' ,'$item_description','$item_locationme', '$item_date', '$user_number', '$img_name')";
+$sql="INSERT INTO items (email, name,type, description, location, date, number, image ) 
+VALUES('".pg_escape_string($email)."',
+'".pg_escape_string($item_name)."',
+'".pg_escape_string($item_type)."' ,
+'".pg_escape_string($item_description)."',
+'".pg_escape_string($item_location)."', 
+'".pg_escape_string($item_date)."',
+'".pg_escape_string($user_number)."',
+'".pg_escape_string($img_name)."' )";
 
 pg_query($conn, $sql);
 header("location: ../discover-items.php");
