b211081e00d43fc8313eb0ac806cf03daa74fe3b
Shatha-gh <shathasaeed2025@gmail.com>
2025-12-16 14:33:53 +0200

    Refactor login query conditions for user authentication

--- phpfiles/loginform.php
+++ phpfiles/loginform.php
@@ -2,8 +2,8 @@
 $conn=pg_connect("host=localhost dbname=lost_found_db user=postgres password=14112006");
 $email = $_POST['email'];
 $password = $_POST['password'];
-if(pg_query($conn, "SELECT name FROM users where email='$email'")){
-    if(pg_query($conn, "SELECT password FROM users where password='$password'")){
+if(pg_query($conn, "SELECT name FROM users WHERE email='$email'")){
+    if(pg_query($conn, "SELECT password FROM users WHERE password='$password'")){
         session_start();
         $_SESSION['email']=$email;
         echo "success";
